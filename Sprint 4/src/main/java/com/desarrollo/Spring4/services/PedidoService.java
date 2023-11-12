1a01311c7a9c73f05f01cdf01815e3fbece06add
BrunoMont-M <142555496+BrunoMont-M@users.noreply.github.com>
2023-11-12 09:26:51 -0300

    Una query más para el GitHub

--- Sprint 4/src/main/java/com/desarrollo/Spring4/services/PedidoService.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/services/PedidoService.java
@@ -1,6 +1,11 @@
 package com.desarrollo.Spring4.services;
 
 import com.desarrollo.Spring4.entities.Pedido;
+import com.desarrollo.Spring4.entities.Usuario;
+
+import java.util.List;
 
 public interface PedidoService extends BaseService<Pedido, Long>{
+
+    List<Pedido> search(String filtro) throws Exception;
 }
--- Sprint 4/src/main/java/com/desarrollo/Spring4/services/PedidoServiceImpl.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/services/PedidoServiceImpl.java
@@ -1,10 +1,13 @@
 package com.desarrollo.Spring4.services;
 
 import com.desarrollo.Spring4.entities.Pedido;
+import com.desarrollo.Spring4.entities.Usuario;
 import com.desarrollo.Spring4.repositories.BaseRepository;
 import com.desarrollo.Spring4.repositories.PedidoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PedidoServiceImpl extends BaseServiceImpl<Pedido, Long> implements PedidoService{
@@ -12,5 +15,15 @@ public class PedidoServiceImpl extends BaseServiceImpl<Pedido, Long> implements 
     private PedidoRepository pedidoRepository;
     public PedidoServiceImpl(BaseRepository<Pedido, Long> baseRepository) {
         super(baseRepository);
+    }
+
+    @Override
+    public List<Pedido> search(String filtro) throws Exception {
+        try {
+            List<Pedido> pedidos = pedidoRepository.searchNativo(filtro);
+            return pedidos;
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        }
     }
 }
