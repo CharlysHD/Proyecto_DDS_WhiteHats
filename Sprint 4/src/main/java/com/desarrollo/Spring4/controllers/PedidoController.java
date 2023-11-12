b08ae1eaaad75bcce4d39474cd62044dcaf50ea8
BrunoMont-M <142555496+BrunoMont-M@users.noreply.github.com>
2023-11-12 09:25:37 -0300

    Una query más para el GitHub

--- Sprint 4/src/main/java/com/desarrollo/Spring4/controllers/PedidoController.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/controllers/PedidoController.java
@@ -2,12 +2,21 @@ package com.desarrollo.Spring4.controllers;
 
 import com.desarrollo.Spring4.entities.Pedido;
 import com.desarrollo.Spring4.services.PedidoServiceImpl;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/pedido")
 public class PedidoController extends BaseControllerImpl<Pedido, PedidoServiceImpl>{
+
+    @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam String filtro) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" +e.getMessage() + "\"}"));
+        }
+    }
 }
