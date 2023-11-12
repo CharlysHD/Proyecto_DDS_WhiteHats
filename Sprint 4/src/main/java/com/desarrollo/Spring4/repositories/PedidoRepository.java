f3bd9efbc627b9f0dc1699b01af2d7a81768f974
BrunoMont-M <142555496+BrunoMont-M@users.noreply.github.com>
2023-11-12 09:26:16 -0300

    Una query más para el GitHub

--- Sprint 4/src/main/java/com/desarrollo/Spring4/repositories/PedidoRepository.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/repositories/PedidoRepository.java
@@ -1,6 +1,15 @@
 package com.desarrollo.Spring4.repositories;
 
 import com.desarrollo.Spring4.entities.Pedido;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PedidoRepository extends BaseRepository<Pedido,Long>{
+
+    @Query(value = "SELECT * FROM pedido WHERE pedido.tipo_envio LIKE %:filtro%",
+            nativeQuery = true
+    )
+    List<Pedido> searchNativo(@Param("filtro") String filtro);
 }
