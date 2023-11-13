ecfecf611c84a934506a4d7b7f63be5d02b077e0
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-13 14:34:33 -0300

    Update EmpleadoService.java

--- Sprint 4/src/main/java/com/desarrollo/Spring4/services/EmpleadoService.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/services/EmpleadoService.java
@@ -3,4 +3,5 @@ package com.desarrollo.Spring4.services;
 import com.desarrollo.Spring4.entities.Empleado;
 
 public interface EmpleadoService extends BaseService<Empleado, Long>{
+  List<Empleado> search(String filtro) throws Exception;
 }
