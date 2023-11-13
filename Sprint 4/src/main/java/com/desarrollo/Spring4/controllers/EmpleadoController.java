0c6f872c512da967f79c2b43e2b65db9b70adc43
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-13 14:32:30 -0300

    Update EmpleadoController.java

--- Sprint 4/src/main/java/com/desarrollo/Spring4/controllers/EmpleadoController.java
+++ Sprint 4/src/main/java/com/desarrollo/Spring4/controllers/EmpleadoController.java
@@ -10,4 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*")
 @RequestMapping(path = "api/v1/empleado")
 public class EmpleadoController extends BaseControllerImpl<Empleado, EmpleadoServiceImpl>{
+  
+  @GetMapping("/search")
+    public ResponseEntity<?> search(@RequestParam String filtro) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK).body(servicio.search(filtro));
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(("{\"error\": \"" +e.getMessage() + "\"}"));
+        }
+    }
 }
