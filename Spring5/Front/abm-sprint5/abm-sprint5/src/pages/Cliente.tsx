950319eb005f178a6ee6c13b24012af451d12657
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-15 17:43:31 -0300

    Add files via upload

---
+++ Spring5/Front/abm-sprint5/abm-sprint5/src/pages/Cliente.tsx
@@ -0,0 +1,11 @@
+import ClientTable from "../components/ClientTable/ClientTable";
+
+export const Administración = () => {
+  return (
+    <>
+      < ClientTable />
+    </>
+  )
+}
+
+export default Administración;
