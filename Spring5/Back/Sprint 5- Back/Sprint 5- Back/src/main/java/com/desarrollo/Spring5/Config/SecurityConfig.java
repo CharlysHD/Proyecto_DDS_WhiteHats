e7972dbab816b970da818ab7210350a4c4ae2cc5
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-22 00:30:51 -0300

    Update SecurityConfig.java

--- Spring5/Back/Sprint 5- Back/Sprint 5- Back/src/main/java/com/desarrollo/Spring5/Config/SecurityConfig.java
+++ Spring5/Back/Sprint 5- Back/Sprint 5- Back/src/main/java/com/desarrollo/Spring5/Config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(List.of("http://localhost:5173"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
         configuration.setAllowedHeaders(List.of("Authorization", "Access-Control-Allow-Origen", "Content-Type", "X-Requested-With", "accept","Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        configuration.setAllowCredentials(false);
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
