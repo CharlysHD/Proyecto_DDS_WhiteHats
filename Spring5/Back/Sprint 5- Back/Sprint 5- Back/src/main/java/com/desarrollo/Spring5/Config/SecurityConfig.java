984cb052741dd490331269a638c1b8463ed15212
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-22 10:16:02 -0300

    Add files via upload

--- Spring5/Back/Sprint 5- Back/Sprint 5- Back/src/main/java/com/desarrollo/Spring5/Config/SecurityConfig.java
+++ Spring5/Back/Sprint 5- Back/Sprint 5- Back/src/main/java/com/desarrollo/Spring5/Config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -39,6 +40,8 @@ public class SecurityConfig {
                         authRequest
                                 //Autenticacion
                                 .requestMatchers(new AntPathRequestMatcher("/auth/**")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/api/**")).permitAll()
+                                
 
                                 //Consola H2:
                                 .requestMatchers(PathRequest.toH2Console()).permitAll()
@@ -46,7 +49,7 @@ public class SecurityConfig {
                                 //Autorizacion de acceso a la url:
                                 .requestMatchers(new AntPathRequestMatcher("/api/v1/demoAdmin/**")).hasAuthority("ADMIN")
                                 .requestMatchers(new AntPathRequestMatcher("/api/v1/demoUser/**")).hasAuthority("USER")
-                                .requestMatchers(new AntPathRequestMatcher("/api/v1/**")).hasAuthority("ADMIN")
+                                .requestMatchers(new AntPathRequestMatcher("/api/v1/producto")).hasAuthority("ADMIN")
                                 .anyRequest().authenticated()
                 )
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)) //H2
@@ -60,15 +63,14 @@ public class SecurityConfig {
     }
 
     @Bean
-   CorsConfigurationSource corsConfigurationSource() {
+    CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Access-Control-Allow-Origen", "Content-Type", "X-Requested-With", "accept","Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Access-Control-Allow-Origen", "Content-Type", "X-Requested-With", "accept","Origin", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
+        configuration.setAllowCredentials(false);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
-
 }
