c76cd7dfcf7f6974dbc8e81c4c29005b400095dd
Sofía Di Francisco <142851934+sofiDiFrancisco@users.noreply.github.com>
2023-11-22 11:27:48 -0300

    ABM con Security

--- Spring5/Front/abm-sprint5/src/services/ProductService.ts
+++ Spring5/Front/abm-sprint5/src/services/ProductService.ts
@@ -2,7 +2,7 @@ import { Producto } from '../types/Product';
 
 
 const BASE_URL = 'https://example-service-fourth.onrender.com';
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+const token = localStorage.getItem('token');
 
 
 export const ProductService = {
